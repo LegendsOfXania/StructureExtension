5375f31e60cdda6f6cc1484f4da69ab008ebc17b
LegendsOfXania <legendsofxania@gmail.com>
2026-01-26 15:50:06 +0100

    MAJ SetStructureActionEntry again

--- extension/src/main/kotlin/fr/legendsofxania/structure/entry/action/SetStructureActionEntry.kt
+++ extension/src/main/kotlin/fr/legendsofxania/structure/entry/action/SetStructureActionEntry.kt
@@ -5,17 +5,13 @@ import com.typewritermc.core.entries.Ref
 import com.typewritermc.core.entries.emptyRef
 import com.typewritermc.core.extension.annotations.Entry
 import com.typewritermc.core.extension.annotations.Help
-import com.typewritermc.core.extension.annotations.Tags
 import com.typewritermc.core.utils.UntickedAsync
 import com.typewritermc.core.utils.launch
 import com.typewritermc.core.utils.point.Position
 import com.typewritermc.engine.paper.entry.Criteria
 import com.typewritermc.engine.paper.entry.Modifier
 import com.typewritermc.engine.paper.entry.TriggerableEntry
-import com.typewritermc.engine.paper.entry.entries.ActionEntry
-import com.typewritermc.engine.paper.entry.entries.ActionTrigger
-import com.typewritermc.engine.paper.entry.entries.ConstVar
-import com.typewritermc.engine.paper.entry.entries.Var
+import com.typewritermc.engine.paper.entry.entries.*
 import com.typewritermc.engine.paper.interaction.interactionContext
 import com.typewritermc.engine.paper.utils.Sync
 import com.typewritermc.engine.paper.utils.toBukkitLocation
@@ -25,24 +21,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.bukkit.block.structure.Mirror
 import org.bukkit.block.structure.StructureRotation
-import org.bukkit.entity.Player
 import java.util.Random
 
-@Entry(
-    "set_structure",
-    "Place a StructureTemplate at a specified location.",
-    Colors.RED,
-    "fluent:apps-48-filled"
-)
-@Tags("set_structure")
-/**
- * The `Set Structure` action entry allows you to place a structure at a specified location.
- *
- * ## How could this be used?
- * This action can be used in various scenarios, such as:
- * - Creating dynamic environments by placing structures based on player actions.
- * - Generating buildings or landmarks in response to game events.
- */
+@Entry("set_structure", "Place a StructureTemplate at a specified location.", Colors.RED, "fluent:apps-48-filled")
 class SetStructureActionEntry(
     override val id: String = "",
     override val name: String = "",
@@ -60,24 +41,21 @@ class SetStructureActionEntry(
 ) : ActionEntry {
     override fun ActionTrigger.execute() {
         Dispatchers.UntickedAsync.launch {
-            setStructure(player)
-        }
-    }
+            val player = player
+            val context = player.interactionContext
+            val structure = template.get(player, context).entry?.let { TemplateManager.loadTemplateAsStructure(it) } ?: return@launch
 
-    private suspend fun setStructure(player: Player) {
-        val templateEntry = template.get(player, player.interactionContext).entry ?: return
-        val structure = TemplateManager.loadTemplateAsStructure(templateEntry) ?: return
-
-        withContext(Dispatchers.Sync) {
-            structure.place(
-                location.get(player, player.interactionContext).toBukkitLocation(),
-                ignoreEntities,
-                rotation.get(player, player.interactionContext),
-                Mirror.NONE,
-                0,
-                1f,
-                Random()
-            )
+            withContext(Dispatchers.Sync) {
+                structure.place(
+                    location.get(player, context).toBukkitLocation(),
+                    ignoreEntities,
+                    rotation.get(player, context),
+                    Mirror.NONE,
+                    0,
+                    1f,
+                    Random()
+                )
+            }
         }
     }
 }
